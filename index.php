ea8679c48ec2b4ae0200cdc167ab7774d786cddc
Dhrubo Raj Roy <Dhruborajroy3@gmail.com>
2021-08-27 09:45:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php 
 $con=mysqli_connect("localhost","root","","DATABASE_NAME");
-$sql="select * from students order by physics, chemistry, math, sum(physics+chemistry+math) as totalMark desc";
+$sql="select * from students order by physics DESC , chemistry DESC, math DESC";
 $res=mysqli_query($con,$sql);
 if(mysqli_num_rows($res)>0){
   $i=1;
   while($row=mysqli_fetch_assoc($res)){
-    mysqli_query($con,"update students set merit='$i' where id='".$row['id']."'");
+    $swl="update students set merit='$i' where id='".$row['id']."'";
+    mysqli_query($con,$swl);
+    echo $row['name']."<br>";
+    echo $row['merit']."<br>";
     $i++;
   }
 }
